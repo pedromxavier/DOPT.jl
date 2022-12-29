b3afe1cab6182d1880d62ee100651645c1b0a3e5
Pedro Maciel Xavier <pedroxavier@psr-inc.com>
2022-12-29 20:37:05 -0300

    Add jobs

--- src/lib/paths.jl
+++ src/lib/paths.jl
@@ -1,17 +1,33 @@
 const DATA_PATH = joinpath(@__DIR__, "..", "..", "data")
 
-function data_path(path::AbstractString...; base_path::AbstractString=DATA_PATH)
+function data_path(path::AbstractString...; base_path::AbstractString = DATA_PATH)
     return abspath(joinpath(base_path, path...))
 end
 
-function instances_path(path::AbstractString...; base_path::AbstractString=DATA_PATH)
+function instances_path(path::AbstractString...; base_path::AbstractString = DATA_PATH)
     return data_path("instances", path...; base_path)
 end
 
-function results_path(path::AbstractString...; base_path::AbstractString=DATA_PATH)
+function results_path(path::AbstractString...; base_path::AbstractString = DATA_PATH)
     return data_path("results", path...; base_path)
 end
 
-function run_path(index::Integer)
+function run_path(run_index::Integer; base_path::AbstractString = results_path())
+    return abspath(joinpath(base_path, "run-$(run_index)"))
+end
 
+function series_path(run_index::Integer; base_path::AbstractString = results_path())
+    return abspath(joinpath(run_path(run_index; base_path = base_path), "series"))
+end
+
+function series_path(
+    run_index::Integer,
+    n::Integer,
+    i::Integer,
+    k::Integer;
+    base_path::AbstractString = results_path(),
+)
+    return abspath(
+        joinpath(series_path(run_index; base_path = base_path), "$(n)_$(i).$(k).csv"),
+    )
 end
