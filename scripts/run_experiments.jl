9f44eeabbbb2e7c5935c32eae8eed1c80027f2e8
Pedro Maciel Xavier <pedroxavier@psr-inc.com>
2022-12-30 18:58:42 -0300

    Run minimal

--- scripts/run_experiments.jl
+++ scripts/run_experiments.jl
@@ -1,26 +1,25 @@
 using DOPT
 
-function run_random()
-    # Test random local search
+function run_minimal()
+    # For TTT-Plots
     t = @timed begin
-        let num_samples = 3, sizes = 100, codes = [1, 2, 3]
+        let num_samples = 3, sizes = 40, codes = 1
             DOPT.run(
                 DOPT.ILS();
                 max_time     = 100.0,
                 max_iter     = 10,
-                max_subiter  = 100,
-                relink_depth = 5,
+                max_subiter  = 1000,
+                relink_depth = 0,
                 num_samples  = num_samples,
                 sizes        = sizes,
                 codes        = codes,
             )
 
             DOPT.run(
-                DOPT.ILS{DOPT.RandomLocalSearch{DOPT.FirstImprovement}}();
-                max_time     = 100.0,
-                max_iter     = 10,
-                max_subiter  = 10_000,
-                relink_depth = 5,
+                DOPT.SimulatedAnnealing();
+                max_iter     = 100,
+                max_subiter  = 100,
+                relink_depth = 0,
                 num_samples  = num_samples,
                 sizes        = sizes,
                 codes        = codes,
@@ -65,47 +64,4 @@ function run_tttplots()
     return nothing
 end
 
-function main()
-    run_tttplots()
-
-    # # With Path Relinking
-    # DOPT.run(
-    #     DOPT.ILS();
-    #     max_time     = 300.0,
-    #     max_iter     = 1000,
-    #     max_subiter  = 1000,
-    #     relink_depth = 5,
-    #     num_samples  = 3,
-    # )
-
-    # DOPT.run(
-    #     DOPT.SimulatedAnnealing();
-    #     max_iter     = 1000,
-    #     max_subiter  = 1000,
-    #     relink_depth = 5,
-    #     num_samples  = 3,
-    # )
-
-    # # Without Path Relinking
-    # DOPT.run(
-    #     DOPT.ILS();
-    #     max_time     = 300.0,
-    #     max_iter     = 1000,
-    #     max_subiter  = 1000,
-    #     relink_depth = 0,
-    #     num_samples  = 3,
-    # )
-
-
-    # DOPT.run(
-    #     DOPT.SimulatedAnnealing();
-    #     max_iter     = 1000,
-    #     max_subiter  = 1000,
-    #     relink_depth = 0,
-    #     num_samples  = 3,
-    # )
-
-    return nothing
-end
-
-main()
+run_minimal()
