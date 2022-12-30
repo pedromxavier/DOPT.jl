be0d3e5f447e23b88dbd32b91847bdc9897c6166
Pedro Maciel Xavier <pedroxavier@psr-inc.com>
2022-12-30 01:00:50 -0300

    Update experiments

--- scripts/run_experiments.jl
+++ scripts/run_experiments.jl
@@ -1,12 +1,16 @@
 using DOPT
 
 function main()
+    # For TTT-Plots
     DOPT.run(
         DOPT.ILS();
         max_time     = 300.0,
         max_iter     =  1000,
         max_subiter  =  1000,
         relink_depth =     5,
+        num_samples  = 10,
+        sizes=100,
+        codes=Integer[1,2,3],
     )
 
     DOPT.run(
@@ -14,21 +18,46 @@ function main()
         max_iter     =       1000,
         max_subiter  =       1000,
         relink_depth =          5,
+        num_samples  = 10,
+        sizes=100,
+        codes=Integer[1,2,3],
     )
 
+    # With Path Relinking
     DOPT.run(
         DOPT.ILS();
         max_time     = 300.0,
         max_iter     =  1000,
         max_subiter  =  1000,
-        relink_depth =     0,
+        relink_depth =     5,
+        num_samples  = 3,
     )
 
     DOPT.run(
         DOPT.SimulatedAnnealing();
         max_iter     =       1000,
         max_subiter  =       1000,
+        relink_depth =          5,
+        num_samples  = 3,
+    )
+
+    # Without Path Relinking
+    DOPT.run(
+        DOPT.ILS();
+        max_time     = 300.0,
+        max_iter     =  1000,
+        max_subiter  =  1000,
+        relink_depth =     0,
+        num_samples  = 3,
+    )
+
+    
+    DOPT.run(
+        DOPT.SimulatedAnnealing();
+        max_iter     =       1000,
+        max_subiter  =       1000,
         relink_depth =          0,
+        num_samples  = 3,
     )
 
     return nothing
